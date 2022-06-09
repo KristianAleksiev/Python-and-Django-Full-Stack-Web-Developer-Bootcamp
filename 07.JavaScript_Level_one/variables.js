82197dc159e6be2e73fcde701ed7c3172ae44459
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-09 19:12:30 +0300

    Add files via upload

---
+++ 07.JavaScript_Level_one/variables.js
@@ -0,0 +1,22 @@
+// VARIABLES
+
+var variableName = value;
+
+
+var bankAccount = 100;
+var deposit = 50;
+
+var total = bankAccount + deposit;
+
+var greeting = "Welcome back:";
+var name = "Kristian";
+
+
+var myVariable // undefined - created, not defined
+var bonus = null; // nothing set to variable on purpose
+
+alert("Hey!") // Pop up in browser
+console.log("Printing in console"); // Printing
+
+prompt("Enter something") // Pop up in browser for input
+var age = prompt("Enter your age")
