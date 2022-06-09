f83b503b73c1e77a201f63572f685029d8ea7ce3
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-09 19:11:29 +0300

    Add files via upload

---
+++ 07.JavaScript_Level_one/for_loops.js
@@ -0,0 +1,21 @@
+// FOR LOOPS IN JAVASCRIPT
+
+
+// For - loops through a number of times
+// For/In - loops through a JS object
+// For/of - loops through arrays
+
+for (var i = 0; i < ; i++) {
+  console.log("Number is " + i);
+}
+
+var word = "ABCDEFGHI";
+for (var i = 0; i < word.length; i++) {
+  console.log(word[i]);
+}
+
+
+var wordTwo = "ABABABABABAB"
+for (var i = 0; i < wordTwo.length; i=i+2) {
+  console.log(wordTwo[i]);
+}
