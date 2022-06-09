74c424d151e64fb5fca7cf296dad44d49eecbcad
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-09 19:12:10 +0300

    Add files via upload

---
+++ 07.JavaScript_Level_one/loops_exercise.js
@@ -0,0 +1,28 @@
+// Loop exercises
+
+// Exercise 1
+var x = 0;
+for (var i = 0; i < 5; i++) {
+  console.log("Hello");
+}
+
+while (x < 5){
+  console.log("Hello");
+  x++;
+}
+
+// Exercise 2
+var num = 1;
+while (num < 25){
+  if (num % 2 !== 0){
+    console.log(num);
+  }
+  num ++
+}
+
+
+for (var i = 0; i < 25; i++) {
+  if (i % 2 !== 0){
+      console.log(i);
+  }
+}
