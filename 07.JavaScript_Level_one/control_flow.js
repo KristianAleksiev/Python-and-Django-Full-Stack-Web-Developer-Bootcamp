34c2184579c9043e066a6471cc79bc9c48bc35e3
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-09 19:10:59 +0300

    Add files via upload

---
+++ 07.JavaScript_Level_one/control_flow.js
@@ -0,0 +1,31 @@
+//CONDITIONAL STATEMENTS IN JS
+
+//if (condition){
+//   code block
+// }
+//
+//
+// if(condition){
+//   code block
+// }else{
+//   code block
+// }
+//
+//
+// if(condition){
+//   code block
+// }else if(condition two){
+//   code block
+// }else{
+//   code block
+// }
+
+
+var hot = false
+var temp = 60
+
+if (temp > 80) {
+  console.log("It's hot outside!")
+}else {
+  console.log("It's not very hot today!")
+}
