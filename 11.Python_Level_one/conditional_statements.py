c318872515a6d43ef103213acee4e5a53371efbe
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-13 21:51:22 +0300

    Add files via upload

---
+++ 11.Python_Level_one/conditional_statements.py
@@ -0,0 +1,15 @@
+if 1 < 2:
+    print("Block one")
+    if 20 < 3:
+        print("Block two")
+    else:
+        print("Block three")
+
+if:
+    pass
+elif:
+    pass
+elif:
+    pass
+else:
+    pass        
