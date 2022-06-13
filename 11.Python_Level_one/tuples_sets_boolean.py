4dc6a150a99674b50d60d3e5a03f829ea0a24747
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-13 21:53:09 +0300

    Add files via upload

---
+++ 11.Python_Level_one/tuples_sets_boolean.py
@@ -0,0 +1,15 @@
+# IMMUTABLE SEQUENCES
+True False
+
+my_tuple = (1, 3, 5)
+print(my_tuple[0])
+
+my_tuple = ("a", True, 123)
+
+# Sets - Unordered, unique elements
+my_set = set()
+my_set.add(1)
+my_set.add(2)
+my_set.add(2)
+my_set.add(5)
+print(my_set)
