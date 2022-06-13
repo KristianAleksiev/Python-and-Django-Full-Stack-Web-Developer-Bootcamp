e64b0a63f48c0db0b6806742a636c57628c31933
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-13 21:53:00 +0300

    Add files via upload

---
+++ 11.Python_Level_one/strings.py
@@ -0,0 +1,15 @@
+my_string = "Hello World!"
+print(my_string)
+print(my_string[-2])
+
+
+print(my_string[2::])
+print(my_string[2:5])
+my_string.upper()
+my_string.lower()
+my_string.title()
+my_string.capitalize()
+my_string.split()
+
+x = "Insert another string: {}".format("INSERT ME")
+print(x)
