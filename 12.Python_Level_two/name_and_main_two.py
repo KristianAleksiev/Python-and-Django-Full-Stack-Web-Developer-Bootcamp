535764c54fabb0b96d8a9926834dd2eaa7f91a58
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-14 18:18:42 +0300

    Add files via upload

---
+++ 12.Python_Level_two/name_and_main_two.py
@@ -0,0 +1,7 @@
+import name_and_main
+print("Top level name_and_main_two.py")
+
+if __name__ == '__main__':
+    print("name_and_main_two.py being run directly")
+else:
+    print("two is being imported")
